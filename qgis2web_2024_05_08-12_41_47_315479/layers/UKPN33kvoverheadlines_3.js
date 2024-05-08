76ef37b6f9970e039413ad76df93e3d200e18593
HansLiBH <libohan1004@hotmail.com>
2024-05-08 16:20:18 +0100

    Delete qgis2web_2024_05_08-12_41_47_315479 directory

--- qgis2web_2024_05_08-12_41_47_315479/layers/UKPN33kvoverheadlines_3.js
+++
@@ -1,1 +0,0 @@
-var json_UKPN33kvoverheadlines_3 = {"type":"FeatureCollection","name":"UKPN33kvoverheadlines_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
